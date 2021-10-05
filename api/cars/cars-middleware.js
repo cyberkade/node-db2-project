9f5a161741696903e02e0a4029b9d8577af89328
Kade Griffith <kadeg95@gmail.com>
2021-10-05 12:56:18 -0600

    completed basic GET requests for the API. installed vin-validator. currently applying middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,9 +1,33 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id)
+    if (car) {
+      req.car = car
+      next()
+    } else {
+      next({ status: 404, message: "car not found" })
+    }
+  } catch (error) {
+    next(error)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { vin, make, model, mileage } = req.body
+  if(!vin) {
+    next({status: 400, message: "vin is missing"})
+  } else if(!make){
+    next({status:400, message: "make is missing"})
+  } else if(!model){
+    next({status:400, message: "model is missing"})
+  } else if(!mileage){
+    next({status:400, message: "mileage is missing"})
+  } else if( vin && make && model && typeof mileage === 'number'){
+    req.validatedCar = req.body
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
@@ -12,4 +36,11 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,26 @@
-// DO YOUR MAGIC
+const express = require('express')
+const Cars = require('./cars-model')
+const {checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique} = require('./cars-middleware')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Cars.getAll()
+        res.status(200).json(cars)
+    }
+    catch (err) {
+        next(err)
+    }
+});
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    try {
+        const car = await Cars.getById(req.params.id)
+        res.status(200).json(car)
+    }
+    catch (err) {
+        next(err)
+    }
+});
+
+module.exports = router
