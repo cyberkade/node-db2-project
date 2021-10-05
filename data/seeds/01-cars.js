886ff92b1dc6ea6f67533d0a2ae0db156685f1b9
Kade Griffith <kadeg95@gmail.com>
2021-10-05 13:29:20 -0600

    added a seed to my db

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,20 @@
-// STRETCH
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert([
+        {
+            "vin": "11111111111111111",
+            "make": "ACURA",
+            "model": "TL 3.2L",
+            "mileage": "120000",
+            "transmission": 'Automatic'
+        },
+        {
+            "vin": "22222222222222222",
+            "make": "FISKER",
+            "model": "KARMA",
+            "mileage": 76555,
+            "title": "Kade Gurf",
+            "transmission": 'Manual'
+        },
+    ])
+  };
